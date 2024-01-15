a2dfbcb93757e3b542f795fad6e771e3ce38c560
Ekat Vas <ekaterinavasilyeva07@gmail.com>
2024-01-15 22:19:36 +0300

    Lesson 1 task 1

---
+++ Lesson_1/1_1/Program.cs
@@ -0,0 +1,10 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+Console.Write("Enter number 1: ");
+int b1 = Convert.ToInt32(Console.ReadLine()!);
+Console.Write("Enter number 2: ");
+int b2 = Convert.ToInt32(Console.ReadLine()!);
+if (b1 * b1 == b2)
+    Console.WriteLine ("Yes");
+else 
+    Console.WriteLine ("No");
