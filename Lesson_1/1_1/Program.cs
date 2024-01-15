9b464b8be6bb4a8b25242e0c34d275292ab0fa70
Ekat Vas <ekaterinavasilyeva07@gmail.com>
2024-01-15 22:37:28 +0300

    Lesson 1 Task 1 some changes

--- Lesson_1/1_1/Program.cs
+++ Lesson_1/1_1/Program.cs
@@ -1,5 +1,7 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
+
+
 Console.Write("Enter number 1: ");
 int b1 = Convert.ToInt32(Console.ReadLine()!);
 Console.Write("Enter number 2: ");
