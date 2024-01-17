4010cda6f579e21436564c6c8623c1748affe824
Ekat Vas <ekaterinavasilyeva07@gmail.com>
2024-01-17 18:30:39 +0300

    Added one more task

---
+++ Lesson_1/1_2/Program.cs
@@ -0,0 +1,14 @@
+﻿// See https://aka.ms/new-console-template for more information
+// Console.WriteLine("Hello, World!");
+// 2. Напишите программу, которая на вход принимает одно число (N), 
+// а на выходе показывает все целые числа в промежутке от -N до N.
+
+Console.WriteLine("Enter a number 1: ");
+int b1 = int.Parse (Console.ReadLine()!);
+int count = -b1;
+while (count <= b1) 
+{
+     Console.Write (count + " ");
+      count++;
+}
+     
